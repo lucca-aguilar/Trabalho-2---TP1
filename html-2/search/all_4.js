abcb01d4fd2604a13a712a11d84634b26f3e63a3
Cálita Uchôa <uchoacalita@gmail.com>
2025-07-06 13:00:46 -0300

    Documentação

---
+++ html-2/search/all_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['listorders_0',['listOrders',['../class_i_s_i.html#a2b181c2b9ea333ad2ec1a2bee20f7dec',1,'ISI']]],
+  ['listwallets_1',['listWallets',['../class_i_s_i.html#abebdcba1cb4260218e0a26f5d692ebb2',1,'ISI']]]
+];
---
+++ html-2/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['create_0',['create',['../class_i_a_c.html#a63fcf540fb1fe617c72b20fdf3709274',1,'IAC::create()'],['../class_i_s_c.html#a4bab6fc1b1a5fcce0f9d09cdcd3e11be',1,'ISC::create()'],['../class_i_s_i.html#a939a57474edb48d128fa6fc312f762da',1,'ISI::create(Wallet &amp;wallet)=0'],['../class_i_s_i.html#aff29842346be3faf2897dff1d42febb9',1,'ISI::create(Order &amp;order)=0']]]
+];
